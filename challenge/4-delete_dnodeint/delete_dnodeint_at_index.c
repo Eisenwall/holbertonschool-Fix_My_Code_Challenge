b986f10273ca6ce6ec5b9873deb62cdad9c289d8
Eisenwall <Eisenwall@proton.me>
2026-02-08 10:30:41 +0000

    HTS

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,55 +2,55 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Deletes a node at a specific index
- * @head: Double pointer to the head of the list
- * @index: Index of the node to delete
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
  *
  * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *saved_head;
 	dlistint_t *tmp;
-	unsigned int i = 0;
+	unsigned int p;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 		return (-1);
 
-	tmp = *head;
+	saved_head = *head;
+	p = 0;
 
-	/* Case 1: Delete the head node (index 0) */
-	if (index == 0)
+	while (p < index && *head != NULL)
 	{
-		*head = tmp->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(tmp);
-		return (1);
+		*head = (*head)->next;
+		p++;
 	}
 
-	/* Traverse the list to find the node at the specified index */
-	/* We use tmp so the original *head pointer stays at the beginning */
-	while (tmp != NULL && i < index)
+	if (p != index)
 	{
-		tmp = tmp->next;
-		i++;
-	}
-
-	/* Case 2: Index is out of range */
-	if (tmp == NULL)
+		*head = saved_head;
 		return (-1);
+	}
 
-	/* Case 3: Delete middle or end node */
-	/* Re-link the 'next' pointer of the previous node */
-	if (tmp->prev != NULL)
-		tmp->prev->next = tmp->next;
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+			tmp->prev = NULL;
+	}
+	else
+	{
+		if ((*head)->prev)
+			(*head)->prev->next = (*head)->next;
 
-	/* Re-link the 'prev' pointer of the next node (if it exists) */
-	if (tmp->next != NULL)
-		tmp->next->prev = tmp->prev;
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
 
-	/* Safe to free the target node now */
-	free(tmp);
-
+		free(*head);
+		*head = saved_head;
+	}
 	return (1);
 }
